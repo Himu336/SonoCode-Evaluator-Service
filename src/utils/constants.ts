afb0f906385082f6d490224fdcd299b62c16400b
Himansh <himanshbansal200521@gmail.com>
2025-08-20 22:36:53 +0530

    ADD: configured a container to run cpp and use cout<<endl in the code for flushing

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const PYTHON_IMAGE= "python:3.8.10-slim";
-export const JAVA_IMAGE= "openjdk:11-jdk-slim"
+export const JAVA_IMAGE= "openjdk:11-jdk-slim";
+export const CPP_IMAGE= "gcc:latest";
 
 //This will represent the header size of the docker stream
 //Docker stream header will contain the data about type of stream
